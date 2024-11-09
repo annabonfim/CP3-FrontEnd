15e95ff3661089eea3c0973ed38a7865ce5a255a
DudaAraujo14 <ma2473574@gmail.com>
2024-11-09 20:42:06 -0300

    feature-javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function (){
     const produtos = document.querySelectorAll(".produto");
 
     produtos.forEach(produto => {
